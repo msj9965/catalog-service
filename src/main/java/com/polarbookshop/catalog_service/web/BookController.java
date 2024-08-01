90103865c3bbf9a4a788b5aba4422dad05d0a263
msj9965 <msj9965@hotmail.com>
2024-08-01 15:54:16 +0900

    검증 및 오류 메시지 처리

--- src/main/java/com/polarbookshop/catalog_service/web/BookController.java
+++ src/main/java/com/polarbookshop/catalog_service/web/BookController.java
@@ -2,6 +2,7 @@ package com.polarbookshop.catalog_service.web;
 
 import com.polarbookshop.catalog_service.domain.Book;
 import com.polarbookshop.catalog_service.domain.BookService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -26,7 +27,7 @@ public class BookController {
     }
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Book post(@RequestBody Book book){
+    public Book post(@Valid @RequestBody Book book){
         return bookService.addBookTocatalog(book);
     }
     @DeleteMapping("{isbn}")
@@ -36,7 +37,7 @@ public class BookController {
     }
 
     @PutMapping("{isbn}")
-    public Book put(@PathVariable String isbn, @RequestBody Book book){
+    public Book put(@PathVariable String isbn, @Valid @RequestBody Book book){
         return bookService.editBookDetails(isbn, book);
     }
 }
