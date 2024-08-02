72bb1685c2585c7ba4c5f2c6d5eb6ad7f3a54995
msj9965 <msj9965@hotmail.com>
2024-08-02 17:24:17 +0900

    ch4-1 중앙식 설정 서버 구현 준비

--- src/main/java/com/polarbookshop/catalog_service/controller/HomeController.java
+++ src/main/java/com/polarbookshop/catalog_service/controller/HomeController.java
@@ -1,12 +1,22 @@
 package com.polarbookshop.catalog_service.controller;
 
+import com.polarbookshop.catalog_service.config.PolarProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+
+    private final PolarProperties polarProperties;
+
+    @Autowired
+    public HomeController(PolarProperties polarProperties) {
+        this.polarProperties = polarProperties;
+    }
+
     @GetMapping
     public String getGreeting(){
-        return "도서 카탈로그에 오신 것을 환영합니다!";
+        return polarProperties.getGreeting();
     }
 }
